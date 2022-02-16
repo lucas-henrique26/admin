ad81673ac796da12d0aeac92463f78ae30468476
Lucas <lucashenriquebranco1@hotmail.com>
2022-02-15 21:59:47 -0300

    dark mode

--- src/components/template/MenuItem.tsx
+++ src/components/template/MenuItem.tsx
@@ -1,27 +1,42 @@
 import Link from 'next/link'
 
 interface MenuItemProps {
-    url: string
+    url?: string
     texto: string
     icone: any
+    className?: string
+    onClick?: (evento: any) => void
 }
 
 export default function MenuItem(props: MenuItemProps) {
-    return (
-        <li className={`hover:bg-gray-100`}>
-            <Link href={props.url}>
-                <a className={`
+    function renderizarLink() {
+        return (
+            <a className={`
                     flex flex-col justify-center items-center
-                    h-20 w-20
+                    h-20 w-20 
+                    dark:text-gray-200
+                    ${props.className}
                 `}>
                 {props.icone}
-                    <span className={`
-                        text-xs font-light text-gray-600
-                    `}>
+                    <span className={`text-xs font-light`}>
                         {props.texto}
                     </span>
-                </a>
-            </Link>
+            </a>
+        )
+        
+    }
+    return (
+        <li onClick={props.onClick} className={`
+            hover:bg-gray-100 dark:hover:bg-gray-800
+            cursor-pointer
+        `}>
+            {props.url ? (
+                <Link href={props.url}>
+                    {renderizarLink()}
+                </Link>
+            ) : (
+                renderizarLink()
+            )}
         </li>
     )
 }
